43f073cec27f60e43901fb0a8d7907501b7b496b
JosueMbunga <118195236+Studiomulex@users.noreply.github.com>
2024-11-30 21:57:38 +0100

    suppression de App.jsx  et le remplace par Home.jsx sous-dossier de pages

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import './App.css';
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-cyan-400">
-      Hello world!
-    </h1>
-  )
-}
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+import './App.css';
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold underline text-cyan-400">
+      Hello world!
+    </h1>
+  )
+}
