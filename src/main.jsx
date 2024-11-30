f9cfdbdc0f3e569f5078f40e05d473fd06a0fe72
JosueMbunga <118195236+Studiomulex@users.noreply.github.com>
2024-11-30 22:51:36 +0100

    importations de routes dans main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/home/Home.jsx'
-
+import { BrowserRouter as Router,Routes,Route } from 'react-router'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <Router>
+       <Routes>
+        <Route path="/" element={<Home/>}></Route>
+       </Routes>
+    </Router>
   </StrictMode>,
 )
