29fe2af48794adcb1c1e3036644bd1cde7dbec2c
JosueMbunga <118195236+Studiomulex@users.noreply.github.com>
2024-12-01 17:49:18 +0100

    utilisation de outlet et link dans mon composant survey

--- src/pages/Survey/Survey.jsx
+++ src/pages/Survey/Survey.jsx
@@ -1,7 +1,11 @@
+import { Outlet,Link } from "react-router"
 export default function Survey(){
   return(
     <div className="bg-red-300 h-dvh w-full">
       <h1>Questionnaire</h1>
+      <Link to="/Client">Questionnaire Client</Link>
+      <Link to="/Freelances">Questionnaire Freelance</Link>
+      <Outlet/>
     </div>
   )
 }
