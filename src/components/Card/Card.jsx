ca7998c62a3e0145f4d6a557511052faace8dfb6
JosueMbunga <118195236+Studiomulex@users.noreply.github.com>
2024-12-03 14:52:22 +0100

    application de la logique styled-commponents

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,10 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
 export default function Card({ label, title}) {
+  const CardLabel=styled.span``
   return (
     <div className="flex flex-col p-6">
-      <span>{label}</span>
+      <CardLabel>{label}</CardLabel>
       {/* <img src={picture} alt="Freelance" className="w-[80px] h-[80px]" /> */}
       <span>{title}</span>
     </div>
