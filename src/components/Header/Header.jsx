7e5d1491ccfa0e0cb5bdd98ed335420aa614b2c7
JosueMbunga <118195236+Studiomulex@users.noreply.github.com>
2024-12-04 21:53:11 +0100

    stylisation de header composant avec du tailwindcss a la place de components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router";
 import styled from "styled-components";
 import colors from "../../utils/style/Color";
+import logo from "../../assets/dark-logo.png";
 const StyledLink = styled(Link)`
   color: #8186a0;
   padding: 15px;
@@ -12,12 +13,15 @@ const StyledLink = styled(Link)`
 `;
 export default function Header() {
   return (
-    <nav>
-      <StyledLink to="/">Accueil</StyledLink>
-      <StyledLink to="/Freelances">Profils</StyledLink>
-      <StyledLink to="/Survey/1" isFullLink>
-        Faire le test
-      </StyledLink>
-    </nav>
+    <div className="flex flex-row bg-white items-center justify-between h-[10dvh]">
+      <img src={logo} alt="" className="w-[100px] h-[40px]" />
+      <nav>
+        <StyledLink to="/">Accueil</StyledLink>
+        <StyledLink to="/Freelances">Profils</StyledLink>
+        <StyledLink to="/Survey/1" isFullLink>
+          Faire le test
+        </StyledLink>
+      </nav>
+    </div>
   );
 }
