905875bc5484fa04ebbfe5c0226eb70453751272
JosueMbunga <118195236+Studiomulex@users.noreply.github.com>
2024-12-28 23:35:05 +0100

    ajout de la gestion du thème dans le composant ThemeProvider avec un état et une fonction pour basculer entre les thèmes

--- src/utils/Context/index.js
+++ src/utils/Context/index.js
@@ -1,14 +1,18 @@
-import {createContext } from 'react';
-import PropTypes from 'prop-types';
+import { createContext, useState } from "react";
+import PropTypes from "prop-types";
 export const ThemeContext = createContext({});
-export const ThemeProvider=({children})=>{
-    return(
-        <ThemeContext.Provider value={{}}>
-            {children}
-        </ThemeContext.Provider>
-    );
-}
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
 
 ThemeProvider.propTypes = {
-    children:PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
