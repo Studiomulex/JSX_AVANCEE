b16a44c6e6d2e6ef1083049fcbc88d35caceb0b3
JosueMbunga <118195236+Studiomulex@users.noreply.github.com>
2024-12-28 23:49:00 +0100

    suppression de la valeur par défaut dans la création du ThemeContext

--- src/utils/Context/index.js
+++ src/utils/Context/index.js
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 import PropTypes from "prop-types";
-export const ThemeContext = createContext({});
+export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState("light");
