134bef1214720b1ae9f6896e63c12c1b0ce1818f
JosueMbunga <118195236+Studiomulex@users.noreply.github.com>
2024-12-28 23:39:38 +0100

    ajout d'un espace vide pour améliorer la lisibilité dans le fichier ThemeProvider

--- src/utils/Context/index.js
+++ src/utils/Context/index.js
@@ -1,6 +1,7 @@
 import { createContext, useState } from "react";
 import PropTypes from "prop-types";
 export const ThemeContext = createContext({});
+
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState("light");
   const toggleTheme = () => {
