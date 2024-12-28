b2a906dee3cdae8c326d73abfeb53ea7dbb82094
JosueMbunga <118195236+Studiomulex@users.noreply.github.com>
2024-12-28 23:30:44 +0100

    ajout du composant ThemeProvider avec validation des props dans utils/Context/index.js

--- src/utils/Context/index.js
+++ src/utils/Context/index.js
@@ -1,2 +1,14 @@
-import { createContext } from 'react';
+import {createContext } from 'react';
+import PropTypes from 'prop-types';
 export const ThemeContext = createContext({});
+export const ThemeProvider=({Children})=>{
+    return(
+        <ThemeContext.Provider value={{}}>
+            {Children}
+        </ThemeContext.Provider>
+    );
+}
+
+ThemeProvider.propTypes = {
+    children:PropTypes.node.isRequired,
+};
