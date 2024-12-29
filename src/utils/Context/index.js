b7cf2a74cb845ec62eb57cd99248c6e7ed8bbe28
JosueMbunga <118195236+Studiomulex@users.noreply.github.com>
2024-12-29 16:04:50 +0100

    remplacement des enfants par le composant Footer dans le ThemeProvider

--- src/utils/Context/index.js
+++ src/utils/Context/index.js
@@ -1,15 +1,16 @@
 import { createContext, useState } from "react";
 import PropTypes from "prop-types";
+import Footer from "../../components/Footer";
 export const ThemeContext = createContext();
 
-export const ThemeProvider = ({ children }) => {
+export const ThemeProvider = () => {
 const [theme,setTheme]=useState('light');
 const toggleTheme=()=>{
   setTheme((prevTheme)=>prevTheme === 'light' ? 'dark' : 'light');
 }
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {children}
+     <Footer/>
     </ThemeContext.Provider>
   );
 };
