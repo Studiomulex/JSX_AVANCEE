0a3de6e44eec3386ff4490e84d4b10ff6062e589
JosueMbunga <118195236+Studiomulex@users.noreply.github.com>
2024-12-29 15:47:20 +0100

    optimisation de la fonction toggleTheme pour utiliser la valeur précédente du thème

--- src/utils/Context/index.js
+++ src/utils/Context/index.js
@@ -3,10 +3,10 @@ import PropTypes from "prop-types";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
-  const toggleTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
-  };
+const [theme,setTheme]=useState('light');
+const toggleTheme=()=>{
+  setTheme((prevTheme)=>prevTheme === 'light' ? 'dark' : 'light');
+}
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       {children}
